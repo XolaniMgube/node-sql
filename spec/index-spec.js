895d94cafc32f0765f4b257fcf733e907923b1b3
XolaniMgube <xnmgube@gmail.com>
2020-05-25 14:24:10 +0200

    1 it statement test works

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,55 +1,38 @@
-const Visitors = require("../src/index")
+describe("the statemet", () => {
+    
+    const Visitors = require('../src/index')
+    let table = new Visitors()
 
+    const visitor = {
+        name: "Xolani",
+        age: 17,
+        date: new Date("3-03-200"),
+        time: "20:00:00",
+        assisted: "Mdiva",
+        comments: "black squad"
+    }
 
+    it('should add new visitors', async() => {
+        const newVisitor = await table.addVisitor(
+            visitor.name,
+            visitor.age,
+            visitor.date,
+            visitor.time,
+            visitor.assisted,
+            visitor.comments
+        )
 
-require('dotenv').config({
-    path: '../.env'
-})
-
-// const cTable = require('console.table')
-// const {
-//     Client
-// } = require("pg");
-
-// const client = new Client({
-//     user: process.env.PGUSER,
-//     host: process.env.PGHOST,
-//     database: process.env.PGDATABASE,
-//     password: process.env.PGPASSWORD,
-//     port: process.env.PGPORT,
-// });
-
-
-
-let visitorTable = new Visitors("Momo", 17, "3-03-200", "20:00", "Mdiva", "black squad")
-visitorInfo = {
-    visitor_name: "Momo",
-    visitor_age: 17,
-    date_of_visit: "3-03-200",
-    time_of_visit: "20:00",
-    assisted_by: "Mdiva",
-    comments: "black squad"
-}
-
-describe("add new visitor function", () => {
-    // it("should check if added info is accurate", async () => {
-    //     results = await visitorTable.addVisitor()
-    //     expect(visitorInfo.visitor_name).toEqual(visitorTable.name)
-    // })
-})
-
-describe("visitor's function have been defined", () => {
-    // it("should check if deleteVisitor is defined", () => {
-    //     expect(visitorTable.viewTable).toBeDefined();
-    // });
-    it("should show the results for of the whole table", () => {
-        async function run() {
-            let data = await visitorTable.viewTable()
-        }
-        
-        
-        expect(run()).toBe("Rapelang")    
+        expect(newVisitor[0].visitor_name).toEqual(visitor.name)
+        expect(newVisitor[0].visitor_age).toEqual(visitor.age);
+		expect(newVisitor[0].date_of_visit).toEqual(visitor.date);
+		expect(newVisitor[0].time_of_visit).toEqual(visitor.time);
+		expect(newVisitor[0].assistant).toEqual(visitor.assistant);
+		expect(newVisitor[0].comments).toEqual(visitor.comments);   
     })
-});
-
+   
+    it("something else", async() => {
+        const seeAllVisitors = await table.viewTable();
+        expect(allVisitors).not.toEqual([])
+    })
+})
 
