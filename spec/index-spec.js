b6a99ba018176b5ac2b75425d3d6824113a4b076
XolaniMgube <xnmgube@gmail.com>
2020-05-26 16:38:36 +0200

    4 it statements running

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,4 +1,4 @@
-describe("the statemet", () => {
+describe("Testing query commands and manipulating database", () => {
     
     const Visitors = require('../src/index')
     let table = new Visitors()
@@ -12,7 +12,7 @@ describe("the statemet", () => {
         comments: "black squad"
     }
 
-    it('should add new visitors', async() => {
+    fit('should add new visitors', async() => {
         const newVisitor = await table.addVisitor(
             visitor.name,
             visitor.age,
@@ -21,18 +21,33 @@ describe("the statemet", () => {
             visitor.assisted,
             visitor.comments
         )
-
         expect(newVisitor[0].visitor_name).toEqual(visitor.name)
-        expect(newVisitor[0].visitor_age).toEqual(visitor.age);
-		expect(newVisitor[0].date_of_visit).toEqual(visitor.date);
-		expect(newVisitor[0].time_of_visit).toEqual(visitor.time);
-		expect(newVisitor[0].assistant).toEqual(visitor.assistant);
-		expect(newVisitor[0].comments).toEqual(visitor.comments);   
+        expect(newVisitor[0].visitor_age).toEqual(visitor.age)
+		expect(newVisitor[0].date_of_visit).toEqual(visitor.date)
+		expect(newVisitor[0].time_of_visit).toEqual(visitor.time)
+		expect(newVisitor[0].assisted_by).toEqual(visitor.assisted)
+		expect(newVisitor[0].comments).toEqual(visitor.comments)  
     })
    
-    it("something else", async() => {
-        const seeAllVisitors = await table.viewTable();
-        expect(allVisitors).not.toEqual([])
+    it("should check if there is visitor data in the table", async() => {
+        const seeAllVisitors = await table.viewTable()
+        expect(seeAllVisitors).not.toEqual([])
+        expect(seeAllVisitors[0].visitor_name).toEqual("Xolani")
+        expect(seeAllVisitors[0].assisted_by).toEqual(visitor.assisted)
     })
+
+    it("should delete one (the first) visitor from the table", async() => {
+        // const visitorID = visitor.
+        const deleteOneVisitor = await table.deleteAVisitor(1)
+        expect(deleteOneVisitor).toEqual([])
+        
+    })
+
+    it("should delete all visitors from the table", async() => {
+        const deleteEveryVisitor = await table.deleteAllVisitors()
+        expect(deleteEveryVisitor).toEqual([])
+    })
+
+
 })
 
